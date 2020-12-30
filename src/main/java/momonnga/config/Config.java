33f16e24b54b4e496cfc0434fad3806de0f10143
momonnga25252 <momonnga25252@gmail.com>
2020-12-30 17:28:01 +0900

    Initial commit

---
+++ src/main/java/momonnga/config/Config.java
@@ -0,0 +1,21 @@
+package momonnga.config;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.*;
+import java.util.logging.Level;
+
+public final class Config extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
